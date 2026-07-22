aa3303a2bc6cca14354abc8e3119d06ae59c5507
Gugens <nero.n262013@gmail.com>
2026-07-22 18:01:22 +0300

    feat: add User model

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(max_length=50, verbose_name='Почта', help_text='Введите адрес почты')
+    telephone = models.CharField(max_length=50, verbose_name='Телефон', help_text='Введите телефон', null=True, blank=True)
+    city = models.CharField(max_length=100, verbose_name='Город', help_text='Введите город', null=True, blank=True)
+    avatar = models.ImageField(upload_to='users/avatars', verbose_name='Аватар', help_text='Загрузите ваш аватар', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+    def __str__(self):
+        return self.username
