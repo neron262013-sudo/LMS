3859c98125bf5b3904d6ee2701be7ec150a192aa
Gugens <nero.n262013@gmail.com>
2026-07-22 18:28:16 +0300

    feat: add Lesson and Course CRUD

--- materials/views.py
+++ materials/views.py
@@ -3,3 +3,33 @@ from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView,
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.object.all()
+    serializer_class = CourseSerializer
+
+
+class LessonCreateApiView(CreateAPIView):
+    queryset = Course.object.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(CreateAPIView):
+    queryset = Course.object.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(CreateAPIView):
+    queryset = Course.object.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateApiView(CreateAPIView):
+    queryset = Course.object.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyApiView(CreateAPIView):
+    queryset = Course.object.all()
+    serializer_class = LessonSerializer
