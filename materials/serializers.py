c874db6d0aae647b24e18b8834de90046e1ec0d6
Gugens <nero.n262013@gmail.com>
2026-07-24 20:16:20 +0300

    feat: lessons count for course

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    number_of_lessons = SerializerMethodField()
+
+    def get_number_of_lessons(self, obj):
+        return Lesson.objects.count()
+
     class Meta:
         model = Course
         fields = "__all__"
