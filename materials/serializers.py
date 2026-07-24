8137e47194714d4ce2d0121ef91fbd6117b2208b
Gugens <nero.n262013@gmail.com>
2026-07-24 21:13:14 +0300

    feat: add lessons to course serializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,18 +4,19 @@ from rest_framework.fields import SerializerMethodField
 from materials.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    number_of_lessons = SerializerMethodField()
-
-    def get_number_of_lessons(self, obj):
-        return Lesson.objects.count()
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
+        fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    number_of_lessons = SerializerMethodField()
+    lessons_in_course = LessonSerializer(source="lesson_set", many=True, read_only=True)
+
+    def get_number_of_lessons(self, obj):
+        return Lesson.objects.filter(course=obj).count()
+
+    class Meta:
+        model = Course
         fields = "__all__"
